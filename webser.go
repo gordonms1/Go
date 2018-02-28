83c735ee2882812189fd3d5007904bd8cbefbbd9
Michaela <gordonms1@icloud.com>
2018-02-28 17:10:30 -0500

    Add files via upload

---
+++ webser.go
@@ -0,0 +1,22 @@
+// Hello Server created using: https://hackernoon.com/how-to-create-a-web-server-in-go-a064277287c9
+package main
+
+import (
+	"net/http"
+	"strings"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Hello " + message
+
+	w.Write([]byte(message))
+}
+
+func main() {
+	http.HandleFunc("/", sayHello)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
